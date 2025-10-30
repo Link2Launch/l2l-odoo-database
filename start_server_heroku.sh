041d05b47fc07069f9da753b99972e463d7271c7
John Damits <kakoijohn@gmail.com>
2025-10-30 16:40:59 +0900

    redo procfile

--- start_server_heroku.sh
+++ start_server_heroku.sh
@@ -1,1 +1,1 @@
-./odoo-bin --db_host c5cqb8h0eop3g3.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:d6r43907kqjbcq --database d6r43907kqjbcq --db_user ub7362fsiabrn8 --db_password pab565bdd6622bcd965f16dc1601882df8a9f417456101606ca73f5cbf2ddabea --addons-path=custom_addons/web,addons
+./odoo-bin --config 'heroku-odoo.conf' --database=d6r43907kqjbcq
