0bacb9cd83fa7148ab6db01a2066ebc7452668db
John Damits <kakoijohn@gmail.com>
2026-01-26 11:46:33 +0900

    update contact model

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,2 @@
-./odoo-bin --config 'heroku-odoo.conf' --http-port $PORT -d link2launch
+# ./odoo-bin --config 'heroku-odoo.conf' --http-port $PORT -d link2launch
+./odoo-bin --config 'heroku-odoo.conf' --http-port 8069 -d link2launch
